9de0c1baa31af520ca60dc91640e349165c15956
jacquelynneheiman1209 <jacquelynneheiman1209@gmail.com>
2023-08-26 09:11:21 -0700

    Removed debug log messages used for testing from the enemy ship

--- SpaceShooter/EnemyShip.cpp
+++ SpaceShooter/EnemyShip.cpp
@@ -11,9 +11,6 @@ EnemyShip::EnemyShip()
 
 bool EnemyShip::initialize()
 {
-	Debug::Log("-------------------------");
-	Debug::Log("Initializing EnemyShip...");
-
 	int randomEnemy = rand() % static_cast<int>(textures.size());
 	
 	if (!enemyTexture.loadFromFile(textures[randomEnemy]))
@@ -33,8 +30,6 @@ bool EnemyShip::initialize()
 
 	enemySprite.setOrigin(origin);
 	enemySprite.setPosition(inactivePosition);
-
-	Debug::Log("Enemy Starting Position: " + inactivePosition.toString());
 
 	return true;
 }
@@ -73,8 +68,6 @@ void EnemyShip::draw(sf::RenderWindow* window)
 
 void EnemyShip::spawn(sf::Vector2f targetPosition, sf::FloatRect gameBounds)
 {
-	Debug::Log("Spawning Enemy Ship...");
-
 	this->gameBounds = gameBounds;
 
 	ScreenEdge randomScreenEdge = static_cast<ScreenEdge>(rand() % 4);
@@ -83,36 +76,29 @@ void EnemyShip::spawn(sf::Vector2f targetPosition, sf::FloatRect gameBounds)
 
 	if (randomScreenEdge == ScreenEdge::LEFT)
 	{
-		Debug::Log("Screen Edge: LEFT");
 		spawnPosition.x = gameBounds.left - 200;
 		spawnPosition.y = (rand() % static_cast<int>((gameBounds.top + gameBounds.height) - 200)) + 100;
 	}
 
 	if (randomScreenEdge == ScreenEdge::RIGHT)
 	{
-		Debug::Log("Screen Edge: RIGHT");
 		spawnPosition.x = (gameBounds.left + gameBounds.width) + 200;
 		spawnPosition.y = (rand() % static_cast<int>((gameBounds.top + gameBounds.height) - 200)) + 100;
 	}
 
 	if (randomScreenEdge == ScreenEdge::TOP)
 	{
-		Debug::Log("Screen Edge: TOP");
 		spawnPosition.x = (rand() % static_cast<int>((gameBounds.left + gameBounds.width))) + 100;
 		spawnPosition.y = gameBounds.top - 200;
 	}
 
 	if (randomScreenEdge == ScreenEdge::BOTTOM)
 	{
-		Debug::Log("Screen Edge: BOTTOM");
 		spawnPosition.x = (rand() % static_cast<int>((gameBounds.left + gameBounds.width))) + 100;
 		spawnPosition.y = (gameBounds.top + gameBounds.height) + 200;
 	}
 
 	enemySprite.setPosition(spawnPosition);
-
-	Debug::Log("Player Position: " + targetPosition.toString());
-	Debug::Log("Enemy Spawn Position: " + spawnPosition.toString());
 }
 
 void EnemyShip::destroy()
@@ -136,11 +122,6 @@ void EnemyShip::move(float deltaTime, sf::Vector2f targetPosition)
 	sf::Vector2f currentPosition = enemySprite.getPosition();
 	currentPosition.x += static_cast<float>(cos((rotation + 90) * (3.14159265 / 180)) * moveSpeed * deltaTime);
 	currentPosition.y += static_cast<float>(sin((rotation + 90) * (3.14159265 / 180)) * moveSpeed * deltaTime);
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl))
-	{
-		Debug::Log("Enemy Ship Position: " + currentPosition.toString());
-	}
 
 	bool isInBoundsX = (currentPosition.x > gameBounds.left && currentPosition.x < (gameBounds.left + gameBounds.width));
 	bool isInBoundsY = (currentPosition.y > gameBounds.top && currentPosition.y < (gameBounds.top + gameBounds.height));
