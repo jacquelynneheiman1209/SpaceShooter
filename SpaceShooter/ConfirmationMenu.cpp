f1cfb13ea6c249d181c062fe044cf4054f56e17d
jacquelynneheiman1209 <jacquelynneheiman1209@gmail.com>
2023-08-28 18:01:06 -0700

    Updated ConfirmationMenu to derive from the Menu base class

--- SpaceShooter/ConfirmationMenu.cpp
+++ SpaceShooter/ConfirmationMenu.cpp
@@ -8,72 +8,39 @@ ConfirmationMenu::ConfirmationMenu() : yesButton(sf::Vector2f(0, 0), "Yes"), noB
 
 bool ConfirmationMenu::initialize(sf::FloatRect windowBounds)
 {
-	// setup background
-	if (!backgroundTexture.loadFromFile("Assets/Graphics/UI/GameOverMenu_Background.png"))
-	{
-		Debug::Log("ConfirmationMenu.cpp : Could not load 'backgroundTexture' from 'Assets/Graphics/UI/GameOverMenu_Background.png'");
-		return false;
-	}
-
-	backgroundSprite.setTexture(backgroundTexture);
-	backgroundSprite.setOrigin(getCenter(backgroundSprite.getLocalBounds()));
-	backgroundSprite.setPosition(getCenter(windowBounds));
-
-	// setup text
-	if (!font.loadFromFile("Assets/Fonts/nulshock.otf"))
+	// setup background sprite & title text
+	if (!Menu::initialize(windowBounds))
 	{
 		return false;
 	}
 
-	sf::FloatRect backgroundBounds = backgroundSprite.getGlobalBounds();
-
-	sf::Vector2f textPosition;
-	textPosition.x = backgroundSprite.getPosition().x;
-	textPosition.y = backgroundBounds.top + 30;
-
-	setText(&titleText, "Are you sure?", 30, textPosition);
-
-	textPosition.y = backgroundSprite.getPosition().y - 20;
-
-	setText(&warningText, "You will lose your progress.", 18, textPosition);
-
-	warningText.setFillColor(sf::Color::Black);
-
-	sf::Vector2f buttonPosition;
-	buttonPosition.x = backgroundBounds.left + (backgroundBounds.width / 4);
-	buttonPosition.y = backgroundBounds.top + 150;
-
-	// setup buttons
-	yesButton = GreenButton(buttonPosition, "Yes");
-
-	buttonPosition.x = backgroundBounds.left + ((backgroundBounds.width / 4) * 3);;
-
-	noButton = RedButton(buttonPosition, "No");
-
-	if (!yesButton.initialize(windowBounds))
+	if (!initializeBackground())
 	{
-		Debug::Log("ConfirmationMenu.cpp : Could not initialize 'yesButton'");
 		return false;
 	}
 
-	if (!noButton.initialize(windowBounds))
+	if (!initializeText())
 	{
-		Debug::Log("ConfirmationMenu.cpp : Could not initialize 'noButton'");
 		return false;
 	}
 
-	yesButton.setScale(.99f, yesButton.getScale().y);
-	noButton.setScale(.99f, noButton.getScale().y);
+	if (!initializeButtons())
+	{
+		return false;
+	}
 
 	return true;
 }
 
 void ConfirmationMenu::draw(sf::RenderWindow* window)
 {
-	window->draw(backgroundSprite);
-	window->draw(titleText);
+	// draw background sprite & title text
+	Menu::draw(window);
+
+	// draw text
 	window->draw(warningText);
 
+	// draw buttons
 	yesButton.draw(window);
 	noButton.draw(window);
 }
@@ -84,20 +51,63 @@ void ConfirmationMenu::handleInput(sf::RenderWindow* window, sf::Event* event)
 	noButton.handleInput(window, event);
 }
 
-sf::Vector2f ConfirmationMenu::getCenter(sf::FloatRect bounds)
+bool ConfirmationMenu::initializeBackground()
 {
-	sf::Vector2f origin;
-	origin.x = bounds.left + (bounds.width / 2);
-	origin.y = bounds.top + (bounds.height / 2);
+	if (!backgroundTexture.loadFromFile("Assets/Graphics/UI/GameOverMenu_Background.png"))
+	{
+		return false;
+	}
 
-	return origin;
+	setupSprite(&backgroundSprite, &backgroundTexture, sf::Vector2f(1, 1), getCenter(gameBounds));
+
+	return true;
 }
 
-void ConfirmationMenu::setText(sf::Text* text, std::string textString, int characterSize, sf::Vector2f textPosition)
+bool ConfirmationMenu::initializeText()
 {
-	text->setFont(font);
-	text->setCharacterSize(characterSize);
-	text->setString(textString);
-	text->setOrigin(getCenter(text->getLocalBounds()));
-	text->setPosition(textPosition);
+	sf::FloatRect backgroundBounds = backgroundSprite.getGlobalBounds();
+
+	sf::Vector2f textPosition;
+	textPosition.x = backgroundSprite.getPosition().x;
+	textPosition.y = backgroundBounds.top + 30;
+
+	setupText(&titleText, "Are you sure?", 30, textPosition);
+
+	textPosition.y = backgroundSprite.getPosition().y - 20;
+
+	setupText(&warningText, "You will lose your progress.", 18, textPosition, sf::Color::Black);
+
+	return true;
+}
+
+bool ConfirmationMenu::initializeButtons()
+{
+	sf::FloatRect backgroundBounds = backgroundSprite.getGlobalBounds();
+
+	// setup yes button
+	sf::Vector2f buttonPosition;
+	buttonPosition.x = backgroundBounds.left + (backgroundBounds.width / 4);
+	buttonPosition.y = backgroundBounds.top + 150;
+
+	yesButton = GreenButton(buttonPosition, "Yes");
+
+	if (!yesButton.initialize(gameBounds))
+	{
+		return false;
+	}
+
+	yesButton.setScale(.99f, yesButton.getScale().y);
+
+	// setup no button
+	buttonPosition.x = backgroundBounds.left + ((backgroundBounds.width / 4) * 3);;
+	noButton = RedButton(buttonPosition, "No");
+
+	if (!noButton.initialize(gameBounds))
+	{
+		return false;
+	}
+
+	noButton.setScale(.99f, noButton.getScale().y);
+
+	return true;
 }
--- SpaceShooter/ConfirmationMenu.h
+++ SpaceShooter/ConfirmationMenu.h
@@ -2,32 +2,29 @@
 #include <SFML/Graphics.hpp>
 #include "GreenButton.h"
 #include "RedButton.h"
+#include "Menu.h"
 
 #ifndef CONFIRMATION_MENU_H
 #define CONFIRMATION_MENU_H
 
-class ConfirmationMenu
+class ConfirmationMenu : public Menu
 {
 public:
 	ConfirmationMenu();
 
-	bool initialize(sf::FloatRect windowBounds);
-	void draw(sf::RenderWindow* window);
-	void handleInput(sf::RenderWindow* window, sf::Event* event);
+	bool initialize(sf::FloatRect windowBounds) override;
+	void draw(sf::RenderWindow* window) override;
+	void handleInput(sf::RenderWindow* window, sf::Event* event) override;
 
 	GreenButton yesButton;
 	RedButton noButton;
 
-private:
-	sf::Texture backgroundTexture;
-	sf::Sprite backgroundSprite;
-
-	sf::Font font;
-	sf::Text titleText;
+protected:
 	sf::Text warningText;
 
-	sf::Vector2f getCenter(sf::FloatRect bounds);
-	void setText(sf::Text* text, std::string textString, int characterSize, sf::Vector2f textPosition);
+	bool initializeBackground() override;
+	bool initializeText() override;
+	bool initializeButtons() override;
 };
 
 #endif // !CONFIRMATION_MENU_H
